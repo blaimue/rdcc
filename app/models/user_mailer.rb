11be41b0fd2db0ba06dae11000132719a8e70c57
Chris Doyle <blaimue@gmail.com>
2010-11-13 17:18:13 -0800

    mailer 'from'

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
   def remember_token(user)
     subject    '[rdcc] Reset password'
     recipients user.email
-    from       "rdcc@heroku.com"
+    from       "info@rdcc.heroku.com"
     sent_on    Time.now
     
     body       :reset_link => "http://rdcc.heroku.com/reset_password/#{user.remember_token}"
